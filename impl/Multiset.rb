1c18aa0fb54f32440f5501d59e1282de9bdc818e
doo2 <admin@example.com>
2015-05-04 21:28:15 +0900

    finally...git add -A

--- impl/Multiset.rb
+++ impl/Multiset.rb
@@ -1,17 +1,28 @@
 class Multiset
-	def initialize
-		set =[]
+	def initialize(n)
+		@set = Array.new(1,n)
+	end
+
+	def show(n)
+		print @set[n]
 	end
 	
 	def contains(n)
-		return set.include? n
+		return @set.include? n
 	end
 
 	def add(n)
-		set.push(n)
+		@set.push(n)
 	end
 
 	def remove(n)
-		
+		for i in 0..@set.size()-1
+			if @set[i]==n
+				@set.delete_at(i)
+				break;
+			end
+		end
+		#@set.delete_at(i)
 	end
 end
+
